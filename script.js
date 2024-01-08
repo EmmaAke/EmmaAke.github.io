d9183048cbba25d02a29070394e31e998c32a7a4
Emmanuel Ahlidza <91508200+EmmaAke@users.noreply.github.com>
2024-01-08 09:49:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//<![CDATA[
-var tlJsHost = ((window.location.protocol == "https:") ? "https://secure.trust-provider.com/" : "http://www.trustlogo.com/");
-document.write(unescape("%3Cscript src='" + tlJsHost + "trustlogo/javascript/trustlogo.js' type='text/javascript'%3E%3C/script%3E"));
-          //]]>
+/*function showContent() {
+            document.getElementById("loading").style.display = "none";
+            document.getElementById("content").style.display = "block";
+        }*/
